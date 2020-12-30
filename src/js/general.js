7795a01f9cb81666fe8459cde3d62d86bf093f36
unknown <089ax@mail.ru>
2020-12-30 20:26:30 +0300

    Тестит формы

--- src/js/general.js
+++ src/js/general.js
@@ -282,7 +282,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // const form = document.querySelector('.popup__form');
 // const offersForm = document.querySelector('.offers__form');
 
-const forms = document.querySelectorAll(`form`);
+// const forms = document.querySelectorAll(`form`);
 
 // ТЕСТ ФОРМ ******************************************************
 
@@ -353,7 +353,9 @@ const forms = document.querySelectorAll(`form`);
 //   })
 //   return result;
 // }
-// const popupThanks = document.querySelector(`.popup-thanks`);
+// const popupThanks = document.querySelector(`.popup--thanks`);
+
+// console.log(popupThanks)
 // const send = (evt) => {
 //   evt.preventDefault();
 //   console.log("test")
@@ -364,8 +366,10 @@ const forms = document.querySelectorAll(`form`);
 //   if (formValidate(target)) {
 //     ajaxSend(global.ajax_url, 'post', formData).then((data) => console.log(data));
 //     target.reset();
+//     new PopupThanks(popupThanks).openPopup()
 //   } else {
 //     console.log("error")
+
 //   }
 // }
 
@@ -374,6 +378,95 @@ const forms = document.querySelectorAll(`form`);
 //     send(evt);
 //   });
 // })
+
+const ajaxSend = (url, method, data) => {
+  return fetch(url, {
+      method: method,
+      headers: {
+          "X-Requested-With": "XMLHttpRequest"
+      },
+      body: data
+  })
+  .then(
+      function (response) {
+          if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+              return;
+          }
+          console.log('success');
+          return response.json();
+      }
+  )
+  .catch(error => console.log(error))
+}
+
+const formValidate = (form) => {
+  const fields = form.elements;
+  let result = true;
+  const phoneRegular = /^\d+$/;
+  [].forEach.call(fields,(el)=> {
+      const parent = el.parentElement;
+      let label = parent.querySelector('label');
+    // Переопределяем если у элемента отсутствует label (кнопки управления, кастомные селекты)
+      if (!label) {
+        // Проверяем элемент на наличие кнопки отправки
+          if (parent.classList.contains('offers__controls')) {
+              return
+          }
+        // Переопределяем в кастомном селекте
+          label = parent.closest('.offers__item').querySelector('label');
+      }
+      const tel = el.classList.contains('field__input--phone');
+      const errorMessage = parent.dataset.message;
+      const labelText = label.getAttribute('data-description');
+      if (
+      (el.required && el.value.length < 1) ||
+      (tel && el.value.length < 18 && !phoneRegular.test(el.value)) ||
+      (el.inputMode === 'email' && el.value.length >= 1 && el.value.indexOf("@") < 1)
+  ) {
+      if (el.value.length < 1) {
+          label.innerHTML = 'Поле должно быть заполнено'
+      } else {
+          label.innerHTML = errorMessage;
+      }
+
+      parent.classList.add('field--no-empty')
+      parent.classList.add('field--error')
+
+      setTimeout(function () {
+          parent.classList.remove('field--error')
+          label.innerHTML = labelText;
+          if (el.value.length === 0) {
+              parent.classList.remove('field--no-empty')
+          }
+      }, 2000);
+        result = false;
+      }
+  })
+return result;
+}
+
+const send = (evt) => {
+  evt.preventDefault();
+  const {target} = evt;
+  const formData = new FormData(target);
+  if (formValidate(target)) {
+      ajaxSend(global.ajax_url, 'post', formData).then((data) => console.log(data));
+      target.reset();
+  } else {
+      console.log("error")
+  }
+}
+
+const forms = document.querySelectorAll(`form`);
+
+forms.forEach(function (item) {
+  item.addEventListener(`submit`, function (evt) {
+      send(evt);
+  })
+})
+
 // ******************************************************************
 
 // Анимация облаков ****************************************************
@@ -394,5 +487,8 @@ if (mapBox) {
 const popups = document.querySelectorAll(`.popup`);
 
 popups.forEach(it => {
+  // if (it.classList.contains(`popup--thanks`)) {
+  //   return
+  // }
   new Popup(it)
 })
