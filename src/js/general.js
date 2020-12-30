43a6eb3c7b32592effdbdd15219de859271f9952
unknown <089ax@mail.ru>
2020-12-30 21:31:53 +0300

    Удаляет дублирование кода

--- src/js/general.js
+++ src/js/general.js
@@ -80,61 +80,6 @@ if(infinity) {
   addInfinity()
 }
 
-
-
-
-// function formValidate(form)
-// {
-//   const fields = form.elements;
-//   let result = true;
-//   const phoneRegular = /^\d+$/;
-//   [].forEach.call(fields,(el)=> {
-//     const parent = el.parentElement;
-//     const label = parent.querySelector('.field__label');
-//     const tel = el.classList.contains('field__input--phone');
-//     const errorMessage = parent.dataset.message;
-//     const labelText = label.textContent;
-//     if (
-//         (el.required && el.value.length < 1) ||
-//         (tel && el.value.length < 18 && !phoneRegular.test(el.value)) ||
-//         (el.inputMode === 'email' && el.value.length >= 1 && el.value.indexOf("@") < 1)
-//       ) {
-//
-//       if(el.value.length < 1) {
-//         label.innerHTML = 'Поле должно быть заполнено'
-//       } else {
-//         label.innerHTML = errorMessage;
-//       }
-//
-//       parent.classList.add('field--no-empty')
-//       parent.classList.add('field--error')
-//
-//       setTimeout(function () {
-//         parent.classList.remove('field--error')
-//         label.innerHTML = labelText;
-//         if(el.value.length === 0) {
-//           parent.classList.remove('field--no-empty')
-//         }
-//       }, 2000);
-//       result = false;
-//     }
-//   })
-//   console.log(result)
-//   return result;
-// }
-
-// const forms = document.querySelectorAll('.popup__form');
-//
-// forms.forEach((item) => {
-//   const button = item.querySelector('.popup__button');
-//
-//   button.addEventListener('click', function (evt) {
-//     evt.preventDefault();
-//     formValidate(item);
-//   })
-// })
-
-
 const stepsButton = document.querySelector(`.how-order__button`);
 const stepsList = document.querySelector(`.steps`);
 
@@ -279,104 +224,9 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-// const form = document.querySelector('.popup__form');
-// const offersForm = document.querySelector('.offers__form');
-
-// const forms = document.querySelectorAll(`form`);
-
 // ТЕСТ ФОРМ ******************************************************
 
-const ajaxSend = (url, method, data) => {
-  return fetch(url, {
-    method: method,
-    headers: {
-      "X-Requested-With": "XMLHttpRequest"
-    },
-    body: data
-  })
-    .then(
-      function (response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-        return response.json();
-      }
-    )
-    .catch(error => console.log(error))
-}
-
-const formValidate = (form) => {
-  const fields = form.elements;
-  let result = true;
-  const phoneRegular = /^\d+$/;
-  [].forEach.call(fields,(el)=> {
-    const parent = el.parentElement;
-    let label = parent.querySelector('label');
-    // Переопределяем если у элемента отсутствует label (кнопки управления, кастомные селекты)
-    if (!label) {
-      // Проверяем элемент на наличие кнопки отправки
-      if(parent.classList.contains('offers__controls')) {
-        return
-      }
-      // Переопределяем в кастомном селекте
-      label = parent.closest('.offers__item').querySelector('label');
-    }
-    const tel = el.classList.contains('field__input--phone');
-    const errorMessage = parent.dataset.message;
-    const labelText = label.getAttribute('data-description');
-    if (
-      (el.required && el.value.length < 1) ||
-      (tel && el.value.length < 18 && !phoneRegular.test(el.value)) ||
-      (el.inputMode === 'email' && el.value.length >= 1 && el.value.indexOf("@") < 1)
-    ) {
-
-      if(el.value.length < 1) {
-        label.innerHTML = 'Поле должно быть заполнено'
-      } else {
-        label.innerHTML = errorMessage;
-      }
-
-      parent.classList.add('field--no-empty')
-      parent.classList.add('field--error')
-
-      setTimeout(function () {
-        parent.classList.remove('field--error')
-        label.innerHTML = labelText;
-        if(el.value.length === 0) {
-          parent.classList.remove('field--no-empty')
-        }
-      }, 2000);
-      result = false;
-    }
-  })
-  return result;
-}
 const popupThanks = document.querySelector(`.popup--thanks`);
-
-const send = (evt) => {
-  evt.preventDefault();
-  console.log("test")
-  const {target} = evt;
-  console.log(target);
-  const formData = new FormData(target);
-  console.log(formData);
-  if (formValidate(target)) {
-    ajaxSend(window.global.ajax_url, 'post', formData).then((data) => console.log(data));
-    target.reset();
-    new PopupThanks(popupThanks).openPopup()
-  } else {
-    console.log("error")
-
-  }
-}
-
-forms.forEach(it => {
-  it.addEventListener('submit', function(evt) {
-    send(evt);
-  });
-})
 
 const ajaxSend = (url, method, data) => {
   return fetch(url, {
@@ -453,6 +303,7 @@ const send = (evt) => {
   if (formValidate(target)) {
       ajaxSend(global.ajax_url, 'post', formData).then((data) => console.log(data));
       target.reset();
+      new PopupThanks(popupThanks).openPopup()
   } else {
       console.log("error")
   }
@@ -486,8 +337,5 @@ if (mapBox) {
 const popups = document.querySelectorAll(`.popup`);
 
 popups.forEach(it => {
-  // if (it.classList.contains(`popup--thanks`)) {
-  //   return
-  // }
   new Popup(it)
 })
